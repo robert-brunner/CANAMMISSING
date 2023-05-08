549ad64bf2478a8c0b02d2a0c7b87b797d1ff042
Robert Brunner <106982994+Anarchist0wl@users.noreply.github.com>
2023-05-07 21:03:30 -0400

    adding basic features

---
+++ src/components/appViews/ApplicationViews.js
@@ -0,0 +1,9 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export const ApplicationViews = () => {
+  return (
+    <Routes>
+      {/* Add your routes here */}
+    </Routes>
+  );
+};
