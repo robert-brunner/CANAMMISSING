6b135c1d187c719a4a70efe0e02ee08397401f03
Robert Brunner <106982994+Anarchist0wl@users.noreply.github.com>
2023-06-15 23:33:22 -0400

    center log

--- src/components/homePage/HomePage.js
+++ src/components/homePage/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RooseveltForest from '../../assets/RooseveltForest.jpg';
+import CanAmMissingLogo from '../../assets/CanAmMissingLogo.png';
 import '../../App.css'; // Import the App.css file
 
 export const HomePage = () => {
@@ -9,7 +10,7 @@ export const HomePage = () => {
         <img src={RooseveltForest} alt="Roosevelt Forest" className="backgroundImage" />
       </div>
       <div className='CanAmLabel'>
-        <img src=''></img>
+        <img src={CanAmMissingLogo} alt="CanAmLogo" className="bckrndLogo"></img>
       </div>
         <div className="greySquare"></div>
     </>
